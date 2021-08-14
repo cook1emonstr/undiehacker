ea5203046ffcf9fd421e18b0123e0601d8f895c8
Andrew Schmitt <andrew@ajschmitt.io>
2021-08-14 15:34:42 -0700

    added nav and updated site formatting for my haikus

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './undies.svg';
+import React from 'react';
 import './App.css';
+import logo from './undies.svg';
+import { NavLink, Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>
-          An Undiespicable Eve Jobs Fansite
-        </h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://www.urbandictionary.com/define.php?term=a%20beard"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          And a Beards are not daughters awareness campaign...
-        </a>
-      </header>
+const Navigation = () => (
+    <nav>
+      <ul>
+        <li><NavLink exact activeClassName="current" to='/'>Home</NavLink></li>
+        <li><NavLink exact activeClassName="current" to='/about'>About</NavLink></li>
+        <li><NavLink exact activeClassName="current" to='/poetry'>Poetry</NavLink></li>
+      </ul>
+    </nav>
+);
+
+const Main = () => (
+    <Switch>
+      <Route exact path='/' component={Home}></Route>
+      <Route exact path='/about' component={About}></Route>
+      <Route exact path='/poetry' component={Poetry}></Route>
+    </Switch>
+);
+
+const Home = () => (
+    <div className='home'>
+        <h1>An Eve Jobs Fansite</h1>
+        <h3>And Haven for poetry enthusiasts and Rosie apologists</h3>
     </div>
-  );
-}
+);
+
+const About = () => (
+    <div className='about'>
+      <h1>About Me</h1>
+      <p>Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum molestias?</p>
+      <p>Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum molestias?</p>
+    </div>
+);
+
+const Poetry = () => (
+    <div className='poetry'>
+      <h1>Poetry</h1>
+        <p>Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum molestias?</p>
+    </div>
+);
+
+const App = () => (
+    <div className='App'>
+        <header className="App-header">
+            <h1>UndieHacker  <img src={logo} className="App-logo" alt="logo" /></h1>
+            <Navigation />
+        </header>
+
+
+    <div className='main-wrapper'>
+        <Main />
+    </div>
+    </div>
+);
 
 export default App;
+
+// import logo from './undies.svg';
+// import './App.css';
+//
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <h1>
+//           An Undiespicable Eve Jobs Fansite
+//         </h1>
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <a
+//           className="App-link"
+//           href="https://www.urbandictionary.com/define.php?term=a%20beard"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           And a Beards are not daughters awareness campaign...
+//         </a>
+//         <a
+//             className="App-link"
+//             href="https://www.urbandictionary.com/define.php?term=a%20beard"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//         >
+//           As well as a haven for avid poetry enthusiasts
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+//
+// export default App;
